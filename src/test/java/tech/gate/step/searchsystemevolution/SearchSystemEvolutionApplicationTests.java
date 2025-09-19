1e4862d1f10cc85429fc0aa73a337b568e921318
JoYongHyeon <whdydgus5778@gmail.com>
2025-09-19 15:32:45 +0900

    first commit

---
+++ src/test/java/tech/gate/step/searchsystemevolution/SearchSystemEvolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.gate.step.searchsystemevolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSystemEvolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
